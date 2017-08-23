6202172eb93e61919d6605bf148d6f2b3e9fdc20
Weslei Freitas <weslei.rfreitas@gmail.com>
2017-08-23 12:15:05 -0300

    feat(app): estudar state and lifecycle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+function FormattedDate(props) {
+    return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <FormattedDate date={this.state.date} />
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+}
+
+ReactDOM.render(
+    <div>
+        <Clock />
+        <Clock />
+        <Clock />
+    </div>,
+    document.getElementById('root')
+);
